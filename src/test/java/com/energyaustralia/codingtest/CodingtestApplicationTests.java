ba8be71b699515bf82ffed6f5bb000aedaa0a1e7
amo31 <amo3167@gmail.com>
2020-07-08 09:58:33 +1000

    1.3 complete

--- src/test/java/com/energyaustralia/codingtest/CodingtestApplicationTests.java
+++ src/test/java/com/energyaustralia/codingtest/CodingtestApplicationTests.java
@@ -1,12 +1,13 @@
 package com.energyaustralia.codingtest;
 
 import com.energyaustralia.codingtest.controller.FestivalsController;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -17,7 +18,7 @@ class CodingtestApplicationTests {
 
     @Test
     void contextLoads()  {
-        Assert.assertNotNull(festivalsController);
+        assertNotEquals(null,festivalsController);
     }
 
 }
